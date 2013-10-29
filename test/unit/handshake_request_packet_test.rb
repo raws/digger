d5bf2e5268df28407e7988d4120ec7159b266654
Ross Paffett <ross@rosspaffett.com>
2013-10-29 18:30:48 -0400

    Add test coverage for generating a handshake request with a session ID

--- test/unit/handshake_request_packet_test.rb
+++ test/unit/handshake_request_packet_test.rb
@@ -2,9 +2,15 @@ require 'test_helper'
 
 module Digger
   class HandshakeRequestPacketTest < TestCase
-    test 'generating a handshake request with the default session ID' do
+    test 'generating a handshake request without a session ID' do
       expected = "\xFE\xFD\x09\x00\x00\x00\x00".force_encoding('ASCII-8BIT')
       packet = HandshakeRequestPacket.new
+      assert_equal expected, packet.to_binary_s
+    end
+
+    test 'generating a handshake request with a session ID' do
+      expected = "\xFE\xFD\x09\x00\x00\x00\x01".force_encoding('ASCII-8BIT')
+      packet = HandshakeRequestPacket.new(session_id: 1)
       assert_equal expected, packet.to_binary_s
     end
   end
